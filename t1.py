2c45261db7a7ee89c9ed2dedf67601cc0ad6590c
MohdPaikerAbbas <paikera33@gmail.com>
2022-07-23 15:26:27 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,11 +1,25 @@
+#DECAGON
 from turtle import *
 
-forward(200)
-left(45)
+left(36)
 forward(100)
-left(45)
-forward(200)
-left(45)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
+forward(100)
+left(36)
 forward(100)
 
-mainloop() #Stop window from closing 
+mainloop()
