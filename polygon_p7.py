9c609ba7bf4a7ce6b3fb52bace2b14370113d417
MohdPaikerAbbas <paikera33@gmail.com>
2022-07-27 11:57:33 +0530

    Create polygon_p7.py

---
+++ polygon_p7.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+penup()
+goto(-300,300)
+dot(20)
+goto(-200,300)
+dot(20)
+goto(-100,300)
+dot(20)
+goto(-250,250)
+dot(20)
+goto(-150,250)
+dot(20)
+goto(-200,200)
+dot(20)
+
+mainloop()
