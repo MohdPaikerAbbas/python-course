787b55788b0c8d4f3888802bab49be2aca3b0a01
MohdPaikerAbbas <paikera33@gmail.com>
2022-07-27 11:57:23 +0530

    Create polygon_p5.py

---
+++ polygon_p5.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+goto(-200,200)
+write('I am here')
+goto(200,200)
+write('I am here')
+goto(200,-200)
+write('  I am here',font=('arial',20,'normal'))
+goto(-200,-200)
+write('I am here')
+mainloop() 
