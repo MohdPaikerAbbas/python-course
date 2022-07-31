6b6f289ba20be4a9a1068dbc2a18a5362ab42066
MohdPaikerAbbas <paikera33@gmail.com>
2022-07-31 11:47:16 +0530

    Create dice_simulator.py

---
+++ dice_simulator.py
@@ -0,0 +1,20 @@
+from random import randint
+win_count=0
+lose_count=0
+
+dice=['1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣']
+
+while True:
+    input("Press Enter to roll dice🎲")
+    out=randint(1,6)
+    print(f'🎲 => {dice[out-1]}')
+    if out==6:
+        win_count+=1
+    elif out==1:
+        lose_count+=1
+    if win_count==3:
+        print("You win")
+        break
+    elif lose_count==3:
+        print("You lose")
+        break
