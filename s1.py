a18d332d4037b2f22d504891edb4a13bae155c15
MohdPaikerAbbas <paikera33@gmail.com>
2022-07-31 11:47:21 +0530

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,14 @@
+# defining Strings in Python
+# all of the following are equivalent
+my_string='Hello'
+print(my_string)
+
+my_string="Hello"
+print(my_string)
+
+my_string='''Hello'''
+print(my_string)
+
+# triple quotes string can extent multiple lines
+my_string="""Hello,Welcome to world of Python"""
+print(my_string)
