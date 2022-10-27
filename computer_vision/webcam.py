b41bc8bfc77dac3cbd4b379ae6a439b794eb2409
MohdPaikerAbbas <paikera33@gmail.com>
2022-10-27 20:15:34 +0530

    updated

---
+++ computer_vision/webcam.py
@@ -0,0 +1,17 @@
+import cv2
+
+webcam = cv2.VideoCapture(0)    #for live webcam=0
+
+while webcam.isOpened():
+    status,frame= webcam.read()
+    if not status:
+        print("Camera not working")
+        break
+    print(frame.shape)
+    print(frame)
+    cv2.imshow("Webcam",frame)
+    #press esc to exit
+    if cv2.waitKey(5) == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
